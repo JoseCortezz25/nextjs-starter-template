37dfe15ac90aa66fce49de7bb2969aaded9aff7f
ajosecortes <alfonsochavarro@flare.com.co>
2024-04-19 16:20:00 -0500

    feat: add initial config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, "src/styles")],
+    prependData: `@import "main.scss"`,
+  },
+};
 
 export default nextConfig;
