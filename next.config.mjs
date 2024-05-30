3b4d2933862625b6b5bdd2a77021ad815ce0c3ec
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-05-29 20:39:32 -0500

    chore: update eslint and sass configuration

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,8 @@ const __dirname = dirname(__filename);
 const nextConfig = {
   sassOptions: {
     includePaths: [join(__dirname, "src/styles")],
-    prependData: `@import "main.scss"`,
-  },
+    prependData: `@import "main.scss";`
+  }
 };
 
 export default nextConfig;
