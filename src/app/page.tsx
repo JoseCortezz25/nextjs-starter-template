52a95297b77b11e53b6a57a40c18b35a61eb5e1e
ajosecortes <alfonsochavarro@flare.com.co>
2025-02-11 10:47:57 -0500

    feat: add Button component and update styles with Tailwind CSS based shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center flex-col">
       Get Started by editing <code className="ml-2 bg-gray-100 rounded-sm p-1">src/app/page.tsx</code>
+      <a href="http://nextjs.org" target="_blank" rel="noopener noreferrer">
+        <Button className="mt-4 cursor-pointer">Learn More about Next.js 15</Button>
+      </a>
     </div>
   );
 }
