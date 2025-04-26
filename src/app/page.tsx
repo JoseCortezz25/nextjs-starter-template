7066df581bea37e2272fe63c2d2329ec6ba99567
ajosecortes <alfonsochavarro@flare.com.co>
2025-04-26 16:22:45 -0500

    Refactor Home component by removing unused button and update import statements in utils.ts for consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
-
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
